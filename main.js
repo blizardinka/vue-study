a8dc489aa5df06de34ccd6939c4dc76f1316fdeb
Serhii Chornoivan <dvjblizardin@gmail.com>
2021-03-04 21:09:57 +0100

    add new tegs to index.html and main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            solar_system: 'sun',
+            planet_1: 'mercury',
+            planet_2: "venus",
+            plaent_3: "earth",
+            description_mercury
+        }
+    }
+})
+
+var description_mercury = "Mercury (0.4 AU from the Sun) is the closest planet to the Sun and on average, all seven other planets.[78][79] The smallest planet in the Solar System (0.055 M⊕), Mercury has no natural satellites."
+
+//Так делать низя 3
+const app_1 = Vue.createApp({
+    data() {
+        return {
+            venus: 'Cool Stuff'
+        }
+    }
+})
