dcab702ac0f9db466ef09a5cae8c5ff3cb9a7c6e
Serhii Chornoivan <dvjblizardin@gmail.com>
2021-03-05 14:48:30 +0100

    3 lesson

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: "./assets/images/socks_green.jpg",
+            link_to_google_translate: "https://translate.google.com/?hl=de" ,
         }
     }
 })
