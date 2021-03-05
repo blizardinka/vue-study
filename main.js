c2fb06e39da23d9b17f966d58fa0d9574ef33399
Serhii Chornoivan <dvjblizardin@gmail.com>
2021-03-05 16:41:23 +0100

    end L5

--- main.js
+++ main.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id: 2234, color: "green"},
+                {id: 2235, color: "blue"},
+            ],
+            sizes: [
+                {id:1234, variant: "35-38" },
+                {id:1235, variant: "39-42"}
+            ],
+            hearthstone: true,
+            cards: ["murloc", "elem", "dragon"],
         }
     }
 })
