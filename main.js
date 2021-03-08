396e0d4df5fae5558c6f96b88ea1b2fdef939633
Serhii Chornoivan <dvjblizardin@gmail.com>
2021-03-08 15:06:25 +0100

    solve task from L8-start

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
+            onSale: false,
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0 ,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: "./assets/images/socks_green.jpg", quantity:50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',
+               quantity:0 },
             ]
         }
     },
@@ -17,8 +18,19 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index
+        },
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
